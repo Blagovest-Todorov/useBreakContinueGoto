6e7cf31004b803be16dcdede7004da7406801359
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-14 10:15:38 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _16.operatorsBreakContinueGoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int outerConter = 0;
+
+            for (int outer = 0; outer <= 10 ; outer++)
+            {
+                for (int inner = 0; inner <= 10; inner++)
+                {
+                    if (inner % 3 == 0)
+                    {
+                        continue;
+                    }
+                    if (outer == 7)
+                    {
+                        break;
+                    }
+                    if ( (inner + outer) > 9 )
+                    {
+                        goto breakOut;
+                    }
+                    
+                }
+                breakOut:
+                outerConter++;
+
+            }
+            
+            Console.WriteLine("Hi" + " " + outerConter);
+        }
+    }
+}
